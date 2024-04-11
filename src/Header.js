8865f4a351ff56d4a52c8c64ac931ac4d6f080e7
Kombobill <billmadru25@gmail.com>
2024-04-11 22:23:26 +0300

    header

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+ 
+
+const Header = () => {
+    return (
+        <>
+        <div className="container-fluid">
+            <div className="row">
+                <div className="d-flex justify-content-center align-items-center w-100 text-uppercase p-3 header">
+                    <i className="fas fa-video"></i> &
+                    nbsp;&nbsp; Movie Central
+
+                </div>
+
+            </div>
+
+        </div>
+        </>
+    )
+} 
